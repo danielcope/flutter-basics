3c1df9345b722bc46afc28bfe7eb5c7ee62003cc
danielcope <daniel.ash.cope@gmail.com>
2022-03-09 13:55:43 -0700

    added a button that calls the increment funciton

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Counter',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.pink,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Counter'),
     );
   }
 }
@@ -102,6 +93,8 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            IconButton(
+                onPressed: _incrementCounter, icon: const Icon(Icons.add)),
           ],
         ),
       ),
